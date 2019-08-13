c177f47c8f131513ca5b0385ef1dc3997c8599e9
Sebastian Sdorra <s.sdorra@gmail.com>
2019-08-13 20:58:45 +0200

    convert footer to local styles

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,28 +1,37 @@
 import React from "react";
+import styled from "styled-components"
 import { Link } from "gatsby";
+
+const DarkFooter = styled.footer`
+background-color: #111;
+`;
+
+const FooterText = styled.p`
+color: #999;
+`;
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <DarkFooter className="footer">
       <div className="content">
-          <div className="container">
-              <div class="columns">
-                <div class="column is-8">
-                    <Link to="/imprint">Imprint & Privacy</Link>
-                 </div>
-                 <div class="column is-4 has-text-right">
-                 <p>
-                      © {new Date().getFullYear()} Plugin Backend of{" "}
-                      <a href="https://scm-manager.org">SCM-Manager</a>
-                    </p>
-                    <p>
-                      Supported by <a href="https://cloudogu.com">Cloudogu GmbH</a>
-                    </p>
-                 </div>
-                </div>
-           </div>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-8">
+              <Link to="/imprint">Imprint & Privacy</Link>
+            </div>
+            <div className="column is-4 has-text-right">
+              <FooterText>
+                © {new Date().getFullYear()} Plugin Backend of{" "}
+                <a href="https://scm-manager.org">SCM-Manager</a>
+              </FooterText>
+              <FooterText>
+                Supported by <a href="https://cloudogu.com">Cloudogu GmbH</a>
+              </FooterText>
+            </div>
+          </div>
+        </div>
       </div>
-    </footer>
+    </DarkFooter>
   );
 };
 
