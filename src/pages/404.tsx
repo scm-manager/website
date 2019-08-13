82b9671884c79a03b4e0f13d1a9c16c8b2740fb6
Sebastian Sdorra <s.sdorra@gmail.com>
2019-08-13 21:37:20 +0200

    improve not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,8 +8,12 @@ import Subtitle from "../components/Subtitle";
 const NotFoundPage = () => (
   <Page>
     <SEO title="404: Not found" />
-    <Title>Not Found</Title>
-    <Subtitle>You just hit a route that doesn&#39;t exist... the sadness.</Subtitle>
+    <div className="container section">
+      <Title>Not Found</Title>
+      <Subtitle>
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </Subtitle>
+    </div>
   </Page>
 );
 
