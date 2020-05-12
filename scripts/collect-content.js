293190afbb28ddc31423612e12f4e22e5997cb10
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-05-12 14:35:15 +0200

    allow authentication by using GITHUB_API_TOKEN environment variable

--- scripts/collect-content.js
+++ scripts/collect-content.js
@@ -6,7 +6,9 @@ const { core } = require("./config");
 
 async function collectContent() {
   const now = Date.now();
-  const api = new Octokit();
+  const api = new Octokit({
+    auth: process.env.GITHUB_API_TOKEN
+  });
   const plugins = await findPlugins();
   console.debug(`Found ${plugins.length} plugin(s): `, plugins);
   console.info(`Collecting content...`);
