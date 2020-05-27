8b743d8b8d4b6b5d07662fe8accd6398f6ff5346
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2020-05-27 11:01:55 +0200

    fix error if no plugin content is present

--- gatsby-node.js
+++ gatsby-node.js
@@ -444,6 +444,10 @@ exports.createSchemaCustomization = ({ actions }) => {
       category: CategoriesYaml!
       documentation: [Documentation]
     }
+    
+    type NavigationYamlFields @infer {
+      plugin: String
+    }
   `;
 
   createTypes(typeDefs);
