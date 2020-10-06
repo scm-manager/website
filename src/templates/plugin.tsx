ed7cdea7768e9e6619b589730380d50f6a7a357d
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-10-06 07:06:48 +0200

    Fix missing readme on plugin detail page

--- src/templates/plugin.tsx
+++ src/templates/plugin.tsx
@@ -23,7 +23,7 @@ export const query = graphql`
       ...PluginLayout
     }
     readme: markdownRemark(
-      fields: { plugin: { eq: $name }, slug: { glob: "**/README" } }
+      fields: { plugin: { eq: $name }, slug: { regex: "/^.*\/README\/?$/" } }
     ) {
       html
     }
