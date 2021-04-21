4721d30f864f098c6044e059e931d0416115a537
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-04-21 10:22:23 +0200

    Upgrade gatsby and plugins to v3 (#34)
    Update gatsby and plugins to v3. Also updated most other dependencies and moved Global CSS declarations to gatsby-brower.js to avoid import order issues.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,5 @@
  */
 
 // You can delete this file if you're not using it
+import "./src/styles/main.scss";
+import "@fortawesome/fontawesome-svg-core/styles.css"
--- src/layout/fontawesome.ts
+++ src/layout/fontawesome.ts
@@ -1,3 +1,2 @@
 import { config } from "@fortawesome/fontawesome-svg-core"
-import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false;
