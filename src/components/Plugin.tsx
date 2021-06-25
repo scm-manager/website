9cfabd54cf05179313d61eee19bb2f7ae71353ba
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-06-25 09:20:52 +0200

    Enable cloudogu plugins (#44)
    Co-authored-by: René Pfeuffer <rene.pfeuffer@cloudogu.com>

--- src/components/Plugin.tsx
+++ src/components/Plugin.tsx
@@ -3,20 +3,36 @@ import { Plugin as PluginType } from "../types/plugin";
 import { navigate } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPuzzlePiece } from "@fortawesome/free-solid-svg-icons";
+import CloudoguLogo from "./CloudoguLogo";
 
 type Props = {
   plugin: PluginType;
 };
 
 const Plugin: FunctionComponent<Props> = ({ plugin }) => {
+  const renderIcon = () => {
+    return (
+      <>
+        {plugin.cloudoguLink ? (
+          <CloudoguLogo size={32} />
+        ) : (
+          <FontAwesomeIcon icon={faPuzzlePiece} size="2x" />
+        )}
+      </>
+    );
+  };
+
+  const onClick = () => {
+    if (plugin.cloudoguLink) {
+      window.open(plugin.cloudoguLink, "_blank");
+    } else {
+      return navigate(`/plugins/${plugin.name}/`);
+    }
+  }
+
   return (
-    <article
-      className="media pointer"
-      onClick={e => navigate(`/plugins/${plugin.name}/`)}
-    >
-      <p className="media-left image is-32x32">
-        <FontAwesomeIcon icon={faPuzzlePiece} size="2x" />
-      </p>
+    <article className="media pointer" onClick={(e) => onClick()}>
+      <p className="media-left image is-32x32">{renderIcon()}</p>
       <div className="media-content">
         <p>
           <strong>{plugin.displayName}</strong>{" "}
