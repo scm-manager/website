60076a89d3c6427640d8f15168e58224b4dc2acf
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-06-16 08:57:25 +0200

    Ignore content from private repos if they cannot be accessed due to missing privileges (#99)

--- scripts/collect-support-branches.js
+++ scripts/collect-support-branches.js
@@ -1,4 +1,5 @@
 const { organization } = require("./config");
+const logger = require("./logger");
 
 const supportBranchRegex = /^support\/(.*)$/;
 
@@ -11,21 +12,29 @@ const supportBranchRegex = /^support\/(.*)$/;
 async function collectSupportBranches(api, repository) {
   const supportBranches = [];
 
-  for await (const branches of api.paginate.iterator(api.repos.listBranches, {
-    owner: organization,
-    repo: repository,
-  })) {
-    for (const branch of branches.data) {
-      const name = branch.name;
-      const regexResult = supportBranchRegex.exec(name);
-      if (regexResult) {
-        const [_matched, range] = regexResult;
-        const branchResult = {
-          range,
-          sha: branch.commit.sha,
-        };
-        supportBranches.push(branchResult);
+  try {
+    for await (const branches of api.paginate.iterator(api.repos.listBranches, {
+      owner: organization,
+      repo: repository,
+    })) {
+      for (const branch of branches.data) {
+        const name = branch.name;
+        const regexResult = supportBranchRegex.exec(name);
+        if (regexResult) {
+          const [_matched, range] = regexResult;
+          const branchResult = {
+            range,
+            sha: branch.commit.sha,
+          };
+          supportBranches.push(branchResult);
+        }
       }
+    }
+  } catch (e) {
+    if (e instanceof Error && 'status' in e && e.status === 404) {
+      logger.warn(`Ignore content because ${repository} could not be found`);
+    } else {
+      throw e;
     }
   }
 
