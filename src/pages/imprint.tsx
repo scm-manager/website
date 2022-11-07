c93074d9bb3bf2e299a9f66fa940e76269ba8549
Florian Scholdei <florian.scholdei@cloudogu.com>
2022-11-07 10:31:24 +0100

    Cleanup styling

--- src/pages/imprint.tsx
+++ src/pages/imprint.tsx
@@ -8,7 +8,7 @@ const ImprintPage = () => (
     <SEO title="Imprint" />
     <Title>Imprint</Title>
     <div className="columns">
-      <div className="column is-three-quarters">
+      <div className="column is-three-quarters content">
         <p>
           Cloudogu GmbH
           <br />
@@ -18,30 +18,22 @@ const ImprintPage = () => (
           <br />
           Germany
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Represented by</strong>
-        </h3>
+        <h3>Represented by</h3>
         <p>
           Cloudogu GmbH, represented by the Managing Director Thomas Grosser
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Contact:</strong>
-        </h3>
+        <h3>Contact:</h3>
         <p>
           Phone: <a href="tel:+4953161808880">+49 (531) 61808880</a>
           <br />
           Fax: <a href="tel:+495312352819">+49 (531) 2352819</a>
           <br />
-          Email: <a href="mailto:&#105;&#110;&#102;&#111;&#64;&#115;&#99;&#109;&#45;&#109;&#97;&#110;&#97;&#103;&#101;&#114;&#46;&#111;&#114;&#103;">
+          Email:{" "}
+          <a href="mailto:&#105;&#110;&#102;&#111;&#64;&#115;&#99;&#109;&#45;&#109;&#97;&#110;&#97;&#103;&#101;&#114;&#46;&#111;&#114;&#103;">
             &#105;&#110;&#102;&#111;&#64;&#115;&#99;&#109;&#45;&#109;&#97;&#110;&#97;&#103;&#101;&#114;&#46;&#111;&#114;&#103;
           </a>
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Register entry</strong>
-        </h3>
+        <h3>Register entry</h3>
         <p>
           Entry in the commercial register.
           <br />
@@ -49,59 +41,68 @@ const ImprintPage = () => (
           <br />
           Register number: HRB 204974
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Value-added tax</strong>
-        </h3>
+        <h3>Value-added tax</h3>
         <p>
-          Value-added tax identification number pursuant to §27(a) German VAT Act (UmstG):
+          Value-added tax identification number pursuant to §27(a) German VAT
+          Act (UmstG):
           <br />
           DE297628722
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Dispute resolution</strong>
-        </h3>
+        <h3>Dispute resolution</h3>
         <p>
-          We are not prepared or obligated to participate in a dispute resolution proceeding before a consumer arbitration board.
+          We are not prepared or obligated to participate in a dispute
+          resolution proceeding before a consumer arbitration board.
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Liability for content</strong>
-        </h3>
+        <h3>Liability for content</h3>
         <p>
-          As a service provider, pursuant to § 7 para. 1 TMG we are responsible for our own content on these web pages according to general laws. However, according to §§ 8 through 10 TMG, we are not obligated to monitor transferred or stored external information or to search for circumstances that indicate illegal activity.
-        </p>
-        <p>
-          Obligations to remove or block the use of information according to general laws remain unaffected by this. Liability in this respect however is only possible from the point in time at which we become aware of a specific infringement of the law. Upon becoming aware of such legal infringements, we will immediately remove this content.
-        </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Liability for links</strong>
-        </h3>
-        <p>
-          Our website contains links to external websites of third parties, over whose content we have no influence. Therefore, we cannot assume any liability for this external content. The respective provider or operator of the pages is always responsible for the contents of the linked pages. The linked pages were checked for possible legal infringements at the time of linking. Illegal content was not recognizable at the time of linking.
+          As a service provider, pursuant to § 7 para. 1 TMG we are responsible
+          for our own content on these web pages according to general laws.
+          However, according to §§ 8 through 10 TMG, we are not obligated to
+          monitor transferred or stored external information or to search for
+          circumstances that indicate illegal activity.
         </p>
         <p>
-          However, permanent monitoring of the content of the linked pages is unreasonable without concrete evidence of a violation of the law. Upon becoming aware of such legal infringements, we will immediately remove this content.
+          Obligations to remove or block the use of information according to
+          general laws remain unaffected by this. Liability in this respect
+          however is only possible from the point in time at which we become
+          aware of a specific infringement of the law. Upon becoming aware of
+          such legal infringements, we will immediately remove this content.
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Copyright</strong>
-        </h3>
+        <h3>Liability for links</h3>
         <p>
-          The contents and works created by the site operators on these pages are subject to German copyright law. Duplication, editing, distribution, and any kind of use outside the limits of copyright law require the written consent of the respective author or creator.
+          Our website contains links to external websites of third parties, over
+          whose content we have no influence. Therefore, we cannot assume any
+          liability for this external content. The respective provider or
+          operator of the pages is always responsible for the contents of the
+          linked pages. The linked pages were checked for possible legal
+          infringements at the time of linking. Illegal content was not
+          recognizable at the time of linking.
         </p>
         <p>
-          The copyrights of third parties are respected for any content on the site that was not created by the operator. In particular, the content of third parties is marked as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. Upon becoming aware of such legal infringements, we will immediately remove this content.
+          However, permanent monitoring of the content of the linked pages is
+          unreasonable without concrete evidence of a violation of the law. Upon
+          becoming aware of such legal infringements, we will immediately remove
+          this content.
         </p>
-        <p>&nbsp;</p>
-        <h3>
-          <strong>Image sources</strong>
-        </h3>
+        <h3>Copyright</h3>
+        <p>
+          The contents and works created by the site operators on these pages
+          are subject to German copyright law. Duplication, editing,
+          distribution, and any kind of use outside the limits of copyright law
+          require the written consent of the respective author or creator.
+        </p>
+        <p>
+          The copyrights of third parties are respected for any content on the
+          site that was not created by the operator. In particular, the content
+          of third parties is marked as such. Should you nevertheless become
+          aware of a copyright infringement, please inform us accordingly. Upon
+          becoming aware of such legal infringements, we will immediately remove
+          this content.
+        </p>
+        <h3>Image sources</h3>
         <p>
           Image index page:{" "}
-          <a href="http://www.freepik.com" title="To Freepik">
+          <a href="https://www.freepik.com/" title="To Freepik">
             Designed by Freepik
           </a>
         </p>
